49f413fffc440d8a31adf0ee6fefd07881726550
Aatif <aatifkhan2001@gmail.com>
2025-08-17 00:00:53 +0530

    Feat : Added Admin routing

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const { userRouter } = require('./routes/user')
 const { courseRouter } = require('./routes/course')
+const { AdminRouter } = require('./routes/admin')
 const app = express()
 const port = 3000
 
 
 app.use('/user',userRouter)
 app.use('/course',courseRouter)
-
+app.use('/admin',AdminRouter)
 
 
 app.get('/course/purchase',(req,res)=>{
