0f1c617aa941695d57b5a5f533bdd2bf8cae08b1
Aatif <aatifkhan2001@gmail.com>
2025-08-19 21:47:15 +0530

    Fix : index js

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const { courseRouter } = require('./routes/course')
 const { AdminRouter } = require('./routes/admin')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
 
-
+app.use(express.json())
 app.use('/user',userRouter)
 app.use('/course',courseRouter)
 app.use('/admin',AdminRouter)
@@ -13,6 +14,8 @@ app.use('/admin',AdminRouter)
 
 async function main(){
    await mongoose.connect("mongodb+srv://aatifk:aatifk2001@cluster0.ji8obbw.mongodb.net/Course-Cart")
+   console.log("Connected to mongo");
+   
    app.listen(port,()=>{
        console.log(`listening on ${port}`);
        
