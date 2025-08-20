34eb8c6f5c457683969aff9202969a41d13826e0
Aatif <aatifkhan2001@gmail.com>
2025-08-20 20:54:31 +0530

    Fix : Configuration

--- config.js
+++ config.js
@@ -1,5 +1,12 @@
+import dotenv from "dotenv"
+dotenv.config()
+
 const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD
 const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD
+
+console.log("ENV JWT_ADMIN_PASSWORD:", process.env.JWT_ADMIN_PASSWORD)
+console.log("ENV JWT_USER_PASSWORD:", process.env.JWT_USER_PASSWORD)
+
 
 export {
     JWT_ADMIN_PASSWORD,
