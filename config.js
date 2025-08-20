9d506c65ca335dbe1828a028c794b1697ed9e86a
Aatif <aatifkhan2001@gmail.com>
2025-08-20 17:44:49 +0530

    Fix : passwords using env variable

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const JWT_USER_PASSWORD = "abbbab"
-const JWT_ADMIN_PASSWORD = "12121adfg"
+const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD
+const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD
 
 export {
     JWT_ADMIN_PASSWORD,
